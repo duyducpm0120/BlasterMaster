1b42f2dbb82feb1604a6e877511de4510a850af0
duyducpm0120 <63636669+ducduypm0120@users.noreply.github.com>
2020-11-25 17:05:26 +0700

    commit something

--- 05-ScenceManager/HealthBar.cpp
+++ 05-ScenceManager/HealthBar.cpp
@@ -37,7 +37,7 @@ void HealthBar::Render()
 			}
 			else
 			{
-				health.at(i)->at(HEALTH_TYPE_PLAYER_UNIT)->Render( x-2, y + 56 - i * HEALTH_SPACE_UNIT,255);
+				health.at(i)->at(HEALTH_TYPE_PLAYER_UNIT)->Render( x-1, y + 56 - i * HEALTH_SPACE_UNIT,255);
 			}
 		}
 	}
