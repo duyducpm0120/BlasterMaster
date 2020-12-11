fd78377c6349ec20ce7eddbe784956041aef4d41
VB000723 <74348188+VB000723@users.noreply.github.com>
2020-12-11 14:46:01 +0700

    Brick OH

--- 05-ScenceManager/OHSophia.cpp
+++ 05-ScenceManager/OHSophia.cpp
@@ -78,7 +78,7 @@ void COHSophia::Update(DWORD dt, vector<LPGAMEOBJECT>* coObjects)
 
 
 		x += min_tx * dx + nx * 0.4f;
-		y += min_ty * dy + (ny < 0 ? ny : 0) * 0.4f;
+		y += min_ty * dy + ny * 0.4f;
 		if (nx != 0) vx = 0;
 		if (ny != 0) vy = 0;
 
